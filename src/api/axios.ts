7a187beb48308f687ae22772e57952cf05f69f27
tnstjd120 <tnstjd120@gmail.com>
2024-08-12 17:48:30 +0900

    feat: Docker 컨테이너 endpoint, storage 리버스 프록시 설정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_API_URL,
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
