a00424af38b8140afb212697a627229f9abd1d8d
tnstjd120 <tnstjd120@gmail.com>
2024-08-12 17:48:18 +0900

    feat: Docker 컨테이너 endpoint, storage 리버스 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_BASE_API_URL,
+          secure: false,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        '/storage': {
+          target: env.VITE_APP_BASE_STORAGE_URL,
+          secure: false,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/storage/, ''),
+        },
+      },
+    },
+  });
+};
