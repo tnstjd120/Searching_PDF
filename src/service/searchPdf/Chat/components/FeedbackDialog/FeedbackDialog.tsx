06f81ce4d8c4d5997769daba4d48f3ef0f250706
tnstjd120 <tnstjd120@gmail.com>
2024-08-13 16:05:45 +0900

    feat: feedback Dialog 컴포넌트 개발

--- src/service/searchPdf/Chat/components/FeedbackDialog/FeedbackDialog.tsx
+++ src/service/searchPdf/Chat/components/FeedbackDialog/FeedbackDialog.tsx
@@ -1,15 +1,26 @@
-import { Button, DialogActions, DialogContentText, DialogTitle, TextField } from '@mui/material';
+import { Button, DialogActions, DialogContentText, DialogTitle } from '@mui/material';
 import * as S from './FeedbackDialog.styled';
+import usePostChatFeedback from '../../hooks/usePostChatFeedback';
+import CircularProgressWithBlur from '@/components/common/Progress/CircularProgressWithBlur';
+import { enqueueSnackbar } from 'notistack';
+import { useRef } from 'react';
 
 interface IFeedbackDialog {
   open: boolean;
   onClose: () => void;
+  engineLogId: number;
 }
 
-// TODO: Feedback POST 만들어야 됨.
-// FIXME: 내부 도커 컨테이너끼리 통신할 수 있게 프록시나 nginx 세팅 해야함.
+const FeedbackDialog = ({ open, onClose, engineLogId }: IFeedbackDialog) => {
+  const { useRequest, isLoading } = usePostChatFeedback();
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-const FeedbackDialog = ({ open, onClose }: IFeedbackDialog) => {
+  const handleClickSend = async () => {
+    await useRequest(engineLogId, textareaRef?.current?.value ?? '');
+    enqueueSnackbar('피드백이 접수되었습니다.', { variant: 'success' });
+    onClose();
+  };
+
   return (
     <S.FeedbackDialog open={open} onClose={onClose}>
       <DialogTitle>Feedback</DialogTitle>
@@ -17,15 +28,17 @@ const FeedbackDialog = ({ open, onClose }: IFeedbackDialog) => {
       <S.FeedbackDialogContent>
         <DialogContentText>현재 답변에 대한 피드백을 적어주세요.</DialogContentText>
 
-        <TextField variant="standard" />
+        <S.Textarea ref={textareaRef} maxRows={8} autoFocus />
       </S.FeedbackDialogContent>
 
       <DialogActions sx={{ color: 'grey' }}>
         <Button color="inherit" onClick={onClose}>
           취소
         </Button>
-        <Button>전송</Button>
+        <Button onClick={handleClickSend}>전송</Button>
       </DialogActions>
+
+      {isLoading && <CircularProgressWithBlur scope="global" />}
     </S.FeedbackDialog>
   );
 };
