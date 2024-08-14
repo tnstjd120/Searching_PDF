9d5f8e393e227a578c9d7961045ce9c9c49b5f76
tnstjd120 <tnstjd120@gmail.com>
2024-08-14 18:09:12 +0900

    feat: Admin 권한 체크 함수 추가

--- src/auth/checkAuth.ts
+++ src/auth/checkAuth.ts
@@ -1,5 +1,16 @@
+import { IUserTokenParsing } from '@/types/User';
+import { jwtDecode } from 'jwt-decode';
+
 export const checkAuth = () => {
   const token = sessionStorage.getItem('token');
   if (token) return true;
   return false;
+};
+
+export const checkAdmin = () => {
+  const token = sessionStorage.getItem('token');
+
+  if (!token) return false;
+  const { userRole } = jwtDecode<Omit<IUserTokenParsing, 'iat' | 'exp'>>(token);
+  return userRole === 'Admin';
 };
